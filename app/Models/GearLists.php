3ddb74018c806d700ebe605a708797b607371e2f
JJStadl2 <69908346+JJStadl2@users.noreply.github.com>
2024-09-14 12:21:33 -0400

    clean up

--- app/Models/GearLists.php
+++ app/Models/GearLists.php
@@ -184,9 +184,7 @@ class GearLists extends Model
 
         $sort = ['item_weight','ASC'];
         $gearListItems = GearListItems::getSortedListItems($gearList->id,$sort,$gearList->uom);
-        Log::debug(__FILE__.' '.__LINE__.' list items for chart: '.print_r($gearListItems,true));
         $categories = DB::table('item_categories')->orderBy('category','asc')->get(['category','value']);
-        Log::debug(__FILE__.' '.__LINE__.' categories for chart: '.print_r($categories,true));
         $listData = [];
         $labels = [];
         $weights =[];
@@ -249,7 +247,6 @@ class GearLists extends Model
         }
 
         $chartData = ['labels'=>$labels,'weights'=>$weights,'colors'=> $chartColors];
-        Log::debug(__FILE__.' '.__LINE__.' chart data for chart: '.print_r($chartData,true));
         return $chartData;
     }
 }
